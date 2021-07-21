7ffeb6150bc667396baf4249e1fca399efd52ede
Hersey Aguilar <herseyaguilar@outlook.com>
2021-07-21 16:21:06 -0600

    fix: clean fields

--- src/screens/create-transaction/index.tsx
+++ src/screens/create-transaction/index.tsx
@@ -1,19 +1,44 @@
-import React, { FC, useState } from 'react';
+import { useNavigation } from '@react-navigation/core';
+import React, { FC, useCallback, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import CategoryPickerItem from 'shared/components/category-picker-item';
 import CurrencyInput from 'shared/components/currency-input';
 import DatePickerItem from 'shared/components/date-picker-item';
 import NotePickerItem from 'shared/components/note-picker-item';
+import { Screens } from 'shared/constants/screens';
+import Transaction from 'shared/models/transaction';
+import ToastService from 'shared/services/toast';
+import { transactionAdded } from 'shared/store/reducers/transaction/reducer';
+import shortid from 'shortid';
+import * as yup from 'yup';
 
-import { useAddTransactionHandler } from './hooks';
 import { ButtonCreate, ButtonText, Container, Pressable, Title } from './styles';
 
 const CreateTransaction: FC = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigation();
   const [ammount, setAmount] = useState(0);
   const [date, setDate] = useState(new Date().toISOString());
   const [category, setCategory] = useState<string>();
   const [notes, setNotes] = useState<string>('');
 
-  const handleCreateTransaction = useAddTransactionHandler({ ammount, date, category, notes });
+  const handleCreateTransaction = useCallback(async () => {
+    try {
+      await schema.validate({ date, ammount, notes, category });
+      dispatch(transactionAdded(new Transaction(shortid.generate(), date, ammount, notes, category!)));
+
+      setAmount(0);
+      setDate(new Date().toISOString());
+      setCategory(undefined);
+      setNotes('');
+
+      navigate.navigate(Screens.DASHBOARD_TAB);
+
+      ToastService.showSuccess('Transaction saved');
+    } catch (err) {
+      ToastService.showError('Please, check the fields');
+    }
+  }, [ammount, category, date, dispatch, navigate, notes]);
 
   return (
     <Container>
@@ -29,6 +54,20 @@ const CreateTransaction: FC = () => {
       </Pressable>
     </Container>
   );
+};
+
+const schema = yup.object().shape({
+  ammount: yup.number().positive().required(),
+  date: yup.date().required(),
+  category: yup.string().required(),
+  notes: yup.string().required().max(128),
+});
+
+type TransactionPayload = {
+  ammount: number;
+  date: string;
+  category?: string;
+  notes: string;
 };
 
 export default CreateTransaction;
